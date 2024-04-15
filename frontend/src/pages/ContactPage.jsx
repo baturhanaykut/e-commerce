2457af0aa2c7f22fd82345ee7590011268c5ee88
Baturhan Aykut <baturhanaykut@gmail.com>
2024-04-15 20:10:32 +0300

    Main Layout yapıldı.

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -1,17 +1,7 @@
-import { Fragment } from "react";
-import Header from "../components/Layout/Header/Header";
 import Contact from "../components/Contact/Contact";
-import Footer from "../components/Layout/Footer/Footer";
-//import Policy from "../components/Layout/Policy/Policy";
+
 const ContactPage = () => {
-  return (
-    <Fragment>
-      <Header />
-      <Contact />
-      {/* <Policy /> */}
-      <Footer />
-    </Fragment>
-  );
+  return <Contact />;
 };
 
 export default ContactPage;
--- frontend/src/pages/ShopPage.jsx
+++ frontend/src/pages/ShopPage.jsx
@@ -1,20 +1,15 @@
 import { Fragment } from "react";
-import Header from "../components/Layout/Header/Header";
 import Categories from "../components/Categories/Categories";
 import Products from "../components/Products/Products";
 import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
-import Footer from "../components/Layout/Footer/Footer";
-//import Policy from "../components/Layout/Policy/Policy";
+
 const ShopPage = () => {
   return (
     <Fragment>
-      <Header />
       <Categories />
       <Products />
       <CampaignSingle />
       <Products />
-      {/* <Policy /> */}
-      <Footer />
     </Fragment>
   );
 };
